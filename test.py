ce2ec65af371312bd5dd74ca637c5d309fad73e5
raduk <radu.kopetz@gmail.com>
2016-03-22 16:14:04 -0700

    Python test

---
+++ test.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+with tf.Graph().as_default(), tf.Session() as sess:
+    a=tf.Variable(tf.random_normal([8192, 8192], stddev=0.35))
+    b=tf.Variable(tf.random_normal([8192, 8192], stddev=0.35))
+    c=tf.matmul(a, b)
+
+    saver = tf.train.Saver()  # defaults to saving all variables
+    sess.run([tf.initialize_all_variables()])
+
+    import time
+    start_time = time.time()
+    sess.run(c)
+    print time.time()-start_time
+
+    tf.train.write_graph(sess.graph_def, './output' , 'test.pb', as_text=False)
